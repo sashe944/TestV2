834975b33b72014c716bc126e95388fea62ff226
sashe944 <sashe944@abv.bg>
2018-03-11 17:17:34 +0200

    Created local database with register, login functionality for students

---
+++ app/src/main/java/com/example/home/androidtestgame/DBHelper.java
@@ -0,0 +1,84 @@
+package com.example.home.androidtestgame;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Home on 10.3.2018 г..
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "TestVersionSecondDataBase.db";
+    public static final int DATABASE_VERSION = 1;
+
+
+    public static final String User = "User";
+    public static final String UserType = "UserType";
+
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE " + User + " (_id INTEGER PRIMARY KEY AUTOINCREMENT,FacultyNumber TEXT,Name TEXT,Gender TEXT,Password TEXT, UserTypeID INTEGER,FOREIGN KEY(UserTypeID) REFERENCES UserType(_id))");
+        db.execSQL("CREATE TABLE " + UserType + "(_id INTEGER PRIMARY KEY,Name TEXT)");
+        db.execSQL("INSERT INTO " + UserType + "(_id,Name) VALUES(1,'Student')");
+    }
+
+    public boolean addUser(String FNumber,String fullName,String password,String userType,String gender) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("FacultyNumber", FNumber);
+        cv.put("Name", fullName);
+        cv.put("Gender", gender);
+        cv.put("Password",password);
+        cv.put("UserTypeID", userType);
+        db.insert("User", null, cv);
+        db.close();
+        return true;
+    }
+
+    public ArrayList<String> getAllUserTypes(){
+        ArrayList<String> userTypes = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + UserType;
+        Cursor cursor = db.rawQuery(selectQuery,null);
+        if(cursor.getCount()>0){
+            while(cursor.moveToNext()){
+                String type = cursor.getString(cursor.getColumnIndex("Name"));
+                userTypes.add(type);
+            }
+        }
+        return userTypes;
+    }
+    public boolean LogInStudent(String password,String fNumber){
+        SQLiteDatabase db = getReadableDatabase();
+       String retrieveQuery = "SELECT * FROM " + User + " WHERE Password= ? AND FacultyNumber= ? ";
+
+        Cursor cursor = db.rawQuery(retrieveQuery,new String[]{password,fNumber});
+        if(cursor.moveToFirst()){
+               password = cursor.getString(cursor.getColumnIndex("Password"));
+               fNumber = cursor.getString(cursor.getColumnIndex("FacultyNumber"));
+        }
+        return true;
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+     db.execSQL("DROP TABLE IF EXISTS User");
+     db.execSQL("DROP TABLE IF EXISTS UserType");
+
+     onCreate(db);
+    }
+
+}
--- app/src/main/java/com/example/home/androidtestgame/LogInActivity.java
+++ app/src/main/java/com/example/home/androidtestgame/LogInActivity.java
@@ -6,18 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
+
+    DBHelper MyHelper;
+
     Button register;
     Button login;
+
+    String studentPassword,studentFaculyNumber;
+    EditText password,fNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        MyHelper = new DBHelper(this);
+
         register = findViewById(R.id.btn_register);
         login = findViewById(R.id.btn_log_in);
+
+        password = findViewById(R.id.et_student_password);
+        fNumber = findViewById(R.id.et_fnumber);
 
         register.setOnClickListener(onClickListener);
         login.setOnClickListener(onClickListener);
@@ -29,6 +41,12 @@ public class LogInActivity extends AppCompatActivity {
 
             Intent intent = null;
             if(v.getId()== R.id.btn_log_in){
+
+                studentFaculyNumber = fNumber.getText().toString();
+                studentPassword = password.getText().toString();
+
+                MyHelper.LogInStudent(studentPassword,studentFaculyNumber);
+
                intent = new Intent(LogInActivity.this,StudentStatusActivity.class);
             }else {
                 intent = new Intent(LogInActivity.this, RegisterActivity.class);
