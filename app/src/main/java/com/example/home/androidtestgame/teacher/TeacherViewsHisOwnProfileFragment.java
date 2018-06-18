689a1bd13c1d9a51866329de48388fcb5b7f482e
sashe944 <sashe944@abv.bg>
2018-06-18 12:52:47 +0300

    changed url for plovdiv router, updated TeacherViewHisOwnProfile to work correctly as needed!

--- app/src/main/java/com/example/home/androidtestgame/teacher/TeacherViewsHisOwnProfileFragment.java
+++ app/src/main/java/com/example/home/androidtestgame/teacher/TeacherViewsHisOwnProfileFragment.java
@@ -3,6 +3,7 @@ package com.example.home.androidtestgame.teacher;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,20 +11,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.home.androidtestgame.App;
 import com.example.home.androidtestgame.R;
 import com.example.home.androidtestgame.constants.Constants;
-import com.example.home.androidtestgame.objects.Subject;
 import com.example.home.androidtestgame.objects.Teacher;
 import com.example.home.androidtestgame.objects.User;
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -31,7 +27,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 
 
 /**
@@ -41,7 +36,6 @@ public class TeacherViewsHisOwnProfileFragment extends Fragment {
 
     Button buttonGoBack;
     TextView teacherName,teacherEmail;
-    String name, email;
     private static final String TAG = "TeacherViewsHisOwnProfi";
 
     public TeacherViewsHisOwnProfileFragment() {
@@ -58,9 +52,6 @@ public class TeacherViewsHisOwnProfileFragment extends Fragment {
         teacherName = teacherProfileView.findViewById(R.id.tvTeacherName);
         teacherEmail = teacherProfileView.findViewById(R.id.tvTeacherEmail);
 
-        name = teacherName.getText().toString();
-        email = teacherEmail.getText().toString();
-
         buttonGoBack = teacherProfileView.findViewById(R.id.btn_back);
         buttonGoBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +65,7 @@ public class TeacherViewsHisOwnProfileFragment extends Fragment {
         return teacherProfileView;
     }
 
-    private class GetTeacherDetailsAsyncTask extends AsyncTask<Void, Void , Void> {
+    private class GetTeacherDetailsAsyncTask extends AsyncTask<Void, Void, String> {
 
         ProgressDialog dialogLogIn =
                 new ProgressDialog(getContext());
@@ -84,6 +75,7 @@ public class TeacherViewsHisOwnProfileFragment extends Fragment {
         public GetTeacherDetailsAsyncTask() {
 
         }
+
 
         @Override
         protected void onPreExecute() {
@@ -95,20 +87,25 @@ public class TeacherViewsHisOwnProfileFragment extends Fragment {
         }
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected String doInBackground(Void... voids) {
             Log.d(TAG, "Getting teacher info from the server");
             URL url;
             HttpURLConnection urlConnection;
             BufferedReader br;
 
             try{
-                url = new URL( Constants.URL + "TeacherViewDetailsServlet" );
+
+                Uri builtUri = Uri.parse(Constants.URL + "TeacherViewDetailsServlet")
+                        .buildUpon()
+                        .appendQueryParameter("id", String.valueOf(App.loggedUserId))
+                        .build();
+
+                url = new URL( builtUri.toString());
 
                 Log.d(TAG, "url: " + url.toString());
                 urlConnection = (HttpURLConnection)
                         url.openConnection();
                 urlConnection.setRequestMethod("GET");
-
 
                 br = new BufferedReader
                         (new InputStreamReader(
@@ -129,20 +126,24 @@ public class TeacherViewsHisOwnProfileFragment extends Fragment {
             }catch(IOException e){
                 Log.wtf("WRONG!", e.getMessage());
             }
-            return null;
+            return result.toString();
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
+        protected void onPostExecute(String result) {
+
             Log.d(TAG, "taking data");
-            Log.d(TAG, "result: " + result);
-            super.onPostExecute(aVoid);
+            Log.d(TAG, "result: " + this.result);
+            super.onPostExecute(result);
             dialogLogIn.dismiss();
 
-            Teacher teacher = new Teacher();
-            teacher.id = (int) App.loggedUserId;
-            teacher.name = name;
-           teacher.email = email;
+            // get server response (teacher details)
+            Teacher teacher = new GsonBuilder().create().fromJson(result, Teacher.class);
+
+            // populate teacher info on UI
+            teacherName.setText(teacher.name);
+            teacherEmail.setText(teacher.email);
+
         }
     }
 
